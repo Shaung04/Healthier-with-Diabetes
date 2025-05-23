43cedd9c8398b76b16cd0bba097bd00691a54d59
Shaung04 <shaung04@comcast.net>
2025-05-23 19:02:32 -0400

    props, and {children} all correctly placed

--- 1onboarding_Screen/components/ui/button.tsx
+++ 1onboarding_Screen/components/ui/button.tsx
@@ -35,21 +35,21 @@ export interface ButtonProps
   asChild?: boolean
 }
  
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, children, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
- 
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      >
-        {children}
-      </Comp>
-    );
-  }
-);
+const Button = React.forwardRef<
+  HTMLButtonElement,
+  ButtonProps
+>(({ className, variant, size, asChild = false, children, ...props }, ref) => {
+  const Comp = asChild ? Slot : "button";
+  return (
+    <Comp
+      className={cn(buttonVariants({ variant, size, className }))}
+      ref={ref}
+      {...props}
+    >
+      {children}
+    </Comp>
+  );
+});
  
 Button.displayName = "Button"
  
